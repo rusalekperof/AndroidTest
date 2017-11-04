267b6cc14a96f12a7f6b2f43045cace2f6c8f999
rusalekperof <rusalekperof@gmail.com>
2017-11-04 17:32:47 +0300

    test1

---
+++ app/src/main/java/com/example/rusal/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rusal.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                throw new RuntimeException();
+            }
+        });
+    }
+}
